d1bdd4cebcc7c75c115115427d315d3e7133d484
Manoranjan Sahoo <manoranjan.sahoo@tanasvi.com>
2022-04-14 08:52:13 +0530

    Make project runnable

--- src/main/java/com/tool/ProjectTool/service/UserService.java
+++ src/main/java/com/tool/ProjectTool/service/UserService.java
@@ -1,6 +1,7 @@
 package com.tool.ProjectTool.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tool.ProjectTool.entity.Users;
 import com.tool.ProjectTool.exception.UserAlreadyExistException;
@@ -9,6 +10,7 @@ import com.tool.ProjectTool.model.request.UpdateUserRequest;
 import com.tool.ProjectTool.model.response.UserResponse;
 import com.tool.ProjectTool.repo.UserRepository;
 
+@Service
 public class UserService {
 
 	@Autowired
